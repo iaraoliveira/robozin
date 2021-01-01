b39c564ed711ec753e71f8b7d5cd6c2d42af7f6d
iaraoliveira <iara99oliveira@gmail.com>
2021-01-01 12:47:11 -0300

    Inicializa repositório e robozin

---
+++ data/readme.md
@@ -0,0 +1,13 @@
+Hello, there
+
+in order to function correctly, this bot need some configuration.
+All you need to do is create a `config.json` inside this folder with the following estructure:
+
+```
+
+{
+    "prefix": "/",
+    "token": "[your-token-goes-here]"
+}
+
+```
---
+++ src/bot.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js')
+
+const config = require('../data/config.json');
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log("Bot logged in")
+})
+
+client.on("message", message => {
+
+    if (message.author.bot) return;
+
+    if (message.content.match(/fala bot/i)) {
+        message.reply("hello there");
+    }
+    
+    if (message.content.match(/ping/i)) {
+        message.channel.send("pong");
+    }
+
+    const words = message.content.split(" ");
+    const command = words[0].slice(1);
+    const args = words.slice(1);
+    const line = args.join(" ");
+       
+    if (message.content.startsWith(config.prefix)) {
+        if (command === "fala") {
+            message.channel.send(line)
+        }
+
+        if (command === "escolhe") {
+            var choice = args[Math.floor(Math.random() * args.length)]
+            message.channel.send(choice)
+        }
+    }
+})
+
+client.login(config.token)
